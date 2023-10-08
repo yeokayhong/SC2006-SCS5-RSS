473c7457ab8164a1fd3352205e58c49ec3948d62
junhao21xd <yaujunhao21@gmail.com>
2023-10-09 00:18:31 +0800

    Update notification_manager.dart

--- scheduler_app/lib/managers/notification_manager.dart
+++ scheduler_app/lib/managers/notification_manager.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:scheduler_app/events.dart';
+import 'package:scheduler_app/entities/route_entity.dart';
 
 class NotificationManager {
   final List<Notification> _notifications = [];
 
   void sendUpdatedNotificationList(){
 
+  }
+
+  List<Notification> getNotificationHistory(){
+      return _notifications;
   }
 
   void _updateNotifications(List<Notification> newNotifications) {
@@ -21,19 +26,20 @@ class NotificationManager {
   
 
   void _removeNotification(Notification toRemove) {
-    if (_notifications.remove(toRemove)) {
-    }
+    _notifications.remove(toRemove);
   }
 
   void _addNotification(Notification toUpdate) {
-    if (_notifications.remove(toUpdate)) {
-    }
-    else {
-  
-    }
+    _notifications.remove(toUpdate);
   }
 }
 
 class Notification {
+  String type;
+  String message;  
+  Route route;
 
+  Notification(
+    {required this.type, 
+    required this.message});
 }
