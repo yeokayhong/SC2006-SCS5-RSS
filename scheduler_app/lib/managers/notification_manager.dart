e3e912326cf30126a8a7301a4b1b335cb348b5ee
junhao21xd <yaujunhao21@gmail.com>
2023-10-11 02:18:33 +0800

    Update notification_manager.dart

--- scheduler_app/lib/managers/notification_manager.dart
+++ scheduler_app/lib/managers/notification_manager.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/services.dart';
+import 'package:scheduler_app/base_classes/notification_enum.dart';
 import 'package:scheduler_app/entities/event_entity.dart';
 import 'package:get_it/get_it.dart';
 import 'package:scheduler_app/entities/route_entity.dart';
 import 'package:scheduler_app/entities/notification_entity.dart';
 import 'package:csv/csv.dart';
+import 'dart:io';
+import 'dart:convert' show utf8;
 
 class NotificationManager {
   final List<Notification> _notifications = [];
 
-  void instantiateNotificationFile(){
-    final _rawData = rootBundle.loadString("assets/NotificationList.csv");
-    List<List<dynamic>> _data = const CsvToListConverter().convert(_rawData);
+  void instantiateNotificationFile() async{
+    final input = File('assets/NotificationList.csv').openRead();
+    final fields = await input.transform(utf8.decoder).transform(CsvToListConverter()).toList();
+    
+    List<Notification> _newNotifications = fields.map((field) {
+      return Notification({
+        field[0], 
+        field[1], 
+        field[2],
+        field[3],
+        field[4],
+      });
+    }).toList();
+    _updateNotifications(_newNotifications);
   }
 
   void updateNotificationFile(){
