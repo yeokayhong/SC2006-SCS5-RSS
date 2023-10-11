74335278606388f20a37da192dd953fd985cc675
russellphun <96657095+russellphun@users.noreply.github.com>
2023-10-11 12:34:09 +0800

    main.dart basic UI implementations

--- scheduler_app/lib/main.dart
+++ scheduler_app/lib/main.dart
@@ -1,125 +1,325 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.red, // Adjust as needed
+//       ),
+//       home: AuthenticationPage(),
+//       //home: LoginPage(),
+//     );
+//   }
+// }
+
+// ... [Your imports and existing code remains the same]
+
+enum AppTab {
+  authentication,
+  mapInput,
+  routeDetails,
+  selectedRoute,
+  // Add more tabs as needed
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  AppTab _currentTab = AppTab.authentication;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: _buildBodyForTab(_currentTab),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.red,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.blue.withOpacity(0.6),
+          currentIndex: _currentTab.index,
+          onTap: (index) {
+            setState(() {
+              _currentTab = AppTab.values[index];
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.login),
+              label: 'Login',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Map Input',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.directions),
+              label: 'Route Details',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle),
+              label: 'Selected Route',
+            ),
+            // Add more items as needed
+          ],
+        ),
+      ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget _buildBodyForTab(AppTab tab) {
+    switch (tab) {
+      case AppTab.authentication:
+        return AuthenticationPage();
+      case AppTab.mapInput:
+        return MapInputPage();
+      case AppTab.routeDetails:
+        return RouteDetailsPage(routeNumber: 1); // Placeholder
+      case AppTab.selectedRoute:
+        return SelectedRoutePage(routeNumber: 1); // Placeholder
+      default:
+        return AuthenticationPage(); // Default page
+    }
   }
+}
 
+// ... [Your existing code for the pages remains the same]
+
+class AuthenticationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            SizedBox(height: 60), // For spacing
+            Icon(Icons.location_on,
+                color: Colors.red, size: 100), // Placeholder icon for the image
+            SizedBox(height: 20),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Travel Scheduler",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 40),
+            Text("Email or Username"),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Uname@mail.com",
+                prefixIcon: Icon(Icons.person_outline),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text("Password"),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: Icon(Icons.lock_outline),
+                suffixIcon: Icon(
+                    Icons.visibility_off), // Placeholder for visibility toggle
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: Text("Forgot Password?"),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Sign in"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red, // Button color
+                onPrimary: Colors.white, // Text color
+                minimumSize: Size(double.infinity, 50), // Button size
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MapInputPage()),
+                );
+              },
+              child: Text("Continue As Guest"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey[300], // Button color
+                onPrimary: Colors.black, // Text color
+                minimumSize: Size(double.infinity, 50), // Button size
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account?"),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Sign up"),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MapInputPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Map Page')),
+      body: Column(
+        children: [
+          Expanded(child: Placeholder()), // This is the placeholder map
+          TextField(decoration: InputDecoration(labelText: 'Origin')),
+          TextField(decoration: InputDecoration(labelText: 'Destination')),
+          ElevatedButton(
+            onPressed: () {
+              _showRouteOptions(context);
+            },
+            child: Text('Show Result'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _showRouteOptions(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Route options'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text('Route 1'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RouteDetailsPage(routeNumber: 1),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Route 2'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RouteDetailsPage(routeNumber: 2),
+                    ),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Route 3'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RouteDetailsPage(routeNumber: 3),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class RouteDetailsPage extends StatelessWidget {
+  final int routeNumber;
+
+  RouteDetailsPage({required this.routeNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Route #$routeNumber Details')),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          Text('Route #$routeNumber',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          // Depending on the routeNumber, you can customize the data shown below
+          Text('Travel Time: ~ 50 mins'),
+          Text('Waiting Time: ~ 5 mins (Train)'),
+          Text('Total Estimated Time: ~ 55 mins (Train)'),
+          //Text('Cost: ~ $1.50'),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      SelectedRoutePage(routeNumber: routeNumber),
+                ),
+              );
+              // Do something when the user selects the route
+            },
+            child: Text('Select Route'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // Navigate back to Map Input Page
+            },
+            child: Text('Return'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SelectedRoutePage extends StatelessWidget {
+  final int routeNumber;
+
+  SelectedRoutePage({required this.routeNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Selected Route #$routeNumber')),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          Placeholder(
+              fallbackHeight: 200.0), // Placeholder for map showing path
+          SizedBox(height: 20),
+          Text('Path for Route #$routeNumber',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          // You can fetch the path details for the route using the routeNumber
+          SizedBox(height: 20),
+          Text('Estimated time remaining: 30 mins'),
+          // This can be dynamic based on the route selected
+        ],
+      ),
     );
   }
 }
