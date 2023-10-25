a7bfa1047904499be722a7ff483275fdbcf82335
Bateauxplanze <82026272+Paraworks@users.noreply.github.com>
2023-10-25 20:03:28 +0800

    Update get_train_service_alerts,get_platform_crowd_density_forecast,get_platform_crowd_density_realtime

--- server/lta_api.py
+++ server/lta_api.py
@@ -5,7 +5,54 @@ class LtaApi:
     def __init__(self, api_key):
         self.api_key = api_key
         self.base_url = "http://datamall2.mytransport.sg/ltaodataservice"
+    
+    def get_train_service_alerts(self):
+        url = f"{self.base_url}/TrainServiceAlerts"
+        headers = {
+            'AccountKey': self.api_key,
+        }
 
+        try:
+            response = requests.get(url, headers=headers)
+
+            if response.status_code == 200:
+                data = response.json()
+                return data['value']  # Assuming the response contains a 'value' key with the alerts
+            else:
+                raise Exception(f"Failed to fetch train service alerts. Status code: {response.status_code}")
+        except Exception as e:
+            raise Exception(f"Failed to get train service alerts: {e}")
+
+
+
+     def get_platform_crowd_density_realtime(self, train_line):
+        url = f"{self.base_url}/PCDRealTime?TrainLine={train_line}"
+        headers = {
+            'AccountKey': self.api_key,
+        }
+
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            raise Exception(f"Failed to fetch platform crowd density real-time: {e}")
+
+    def get_platform_crowd_density_forecast(self, train_line):
+        url = f"{self.base_url}/PCDForecast?TrainLine={train_line}"
+        headers = {
+            'AccountKey': self.api_key,
+        }
+
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            raise Exception(f"Failed to fetch platform crowd density forecast: {e}")
+
+    
+    
     def get_estimated_waiting_time(self, bus_stop_code, service_no=""):
         url = f"{self.base_url}/BusArrivalv2?BusStopCode={bus_stop_code}&ServiceNo={service_no}"
         headers = {
